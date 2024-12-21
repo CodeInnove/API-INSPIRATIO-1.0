b20f0a935ae67dbe5f287495f0a14dbccf43faa5
LucasVannucchi <lucasvannucchi16@gmail.com>
2024-12-21 04:43:00 -0300

    create intraverbalAssessment

--- src/app.module.ts
+++ src/app.module.ts
@@ -20,6 +20,7 @@ import { MchatRModule } from './features/graphics/mchat-r/mchat-r.module';
 import { AssessmentOfFunctionalLivingSkillsModule } from './features/graphics/assessment-of-functional-living-skills/assessment-of-functional-living-skills.module';
 import { VerbalBehaviorMilestonesAssessmentAndPlacementProgramModule } from './features/graphics/verbal-behavior-milestones-assessment-and-placement-program/verbal-behavior-milestones-assessment-and-placement-program.module';
 import { ScaleAsrsModule } from './features/scale-asrs/scaleASRS.module';
+import { IntraverbalAssessmentModule } from './features/intraverbal-assessment/intraverbalAssessment.module';
 
 @Module({
   imports: [
@@ -79,6 +80,7 @@ import { ScaleAsrsModule } from './features/scale-asrs/scaleASRS.module';
     AssessmentOfFunctionalLivingSkillsModule,
     VerbalBehaviorMilestonesAssessmentAndPlacementProgramModule,
     ScaleAsrsModule,
+    IntraverbalAssessmentModule,
   ],
   controllers: [AppController],
   providers: [AppService],
