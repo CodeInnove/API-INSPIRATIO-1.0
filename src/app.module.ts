d4d25ea5f651af59ca427b448e0227544aea4572
Gabriel Henrique Stamato <stamato7@gmail.com>
2024-12-23 12:27:54 -0300

    feat: add afls (#8)

--- src/app.module.ts
+++ src/app.module.ts
@@ -17,9 +17,11 @@ import { FunctionalIndependencyMetricsModule } from './features/graphics/functio
 import { childhoodAutismRatingScaleModule } from './features/graphics/childhoodAutismRatingScale/childhoodAutismRatingScale.module';
 import { AnamneseGeneralModule } from './features/reports/anamnese_general/anamnese_general.module';
 import { MchatRModule } from './features/graphics/mchat-r/mchat-r.module';
+import { AflsModule } from './features/afls/afls.module';
 import { AssessmentOfFunctionalLivingSkillsModule } from './features/graphics/assessment-of-functional-living-skills/assessment-of-functional-living-skills.module';
 import { VerbalBehaviorMilestonesAssessmentAndPlacementProgramModule } from './features/graphics/verbal-behavior-milestones-assessment-and-placement-program/verbal-behavior-milestones-assessment-and-placement-program.module';
 import { ScaleAsrsModule } from './features/scale-asrs/scaleASRS.module';
+
 
 @Module({
   imports: [
@@ -76,6 +78,7 @@ import { ScaleAsrsModule } from './features/scale-asrs/scaleASRS.module';
     childhoodAutismRatingScaleModule,
     AnamneseGeneralModule,
     MchatRModule,
+    AflsModule,
     AssessmentOfFunctionalLivingSkillsModule,
     VerbalBehaviorMilestonesAssessmentAndPlacementProgramModule,
     ScaleAsrsModule,
---
+++ src/features/afls/types/vocationals.ts
@@ -0,0 +1,523 @@
+export interface IVocationalsSkills {
+  selfAwarenessCareerExplorationAndJobSearchSkills: SelfAwarenessCareerExplorationAndJobSearchSkills;
+  interviewPreparationAndExecutionSkills: InterviewPreparationAndExecutionSkills;
+  workplaceSkills: WorkplaceSkills;
+  workplaceInteractionSkills: WorkplaceInteractionSkills;
+  workplaceSafetySkills: WorkplaceSafetySkills;
+  materialHandlingAndManualTasksSkills: MaterialHandlingAndManualTasksSkills;
+  cleaningAndMaintenanceSkills: CleaningAndMaintenanceSkills;
+  laundrySkills: LaundrySkills;
+  retailSkills: RetailSkills;
+  recreationAndChildcareSkills: RecreationAndChildcareSkills;
+  officeSkills: OfficeSkills;
+  computerSkills: ComputerSkills;
+  foodServiceSkills: FoodServiceSkills;
+  foodPreparationSkills: FoodPreparationSkills;
+  warehouseSkills: WarehouseSkills;
+  manualLaborSkills: ManualLaborSkills;
+  maintenanceSkills: MaintenanceSkills;
+  gardeningSkills: GardeningSkills;
+}
+
+interface SelfAwarenessCareerExplorationAndJobSearchSkills {
+  identifiesOwnStrengthsAndWeaknesses: number;
+  identifiesAreasOfInterestForWork: number;
+  identifiesJobsThatMatchAreasOfInterest: number;
+  identifiesQualitiesOrAttributesThatWouldHelpInCertainJobsOrPositions: number;
+  providesAClearDescriptionOfSpecificTypesOfWork: number;
+  recognizesJobPostingsInTheCommunity: number;
+  inquiresAboutEmployment: number;
+  jobSearchNetworking: number;
+  makesCallsToInquireAboutEmployment: number;
+  sendsEmailsToInquireAboutThePosition: number;
+  searchesForNewJobOpenings: number;
+  understandsAbbreviationsInJobAdvertisements: number;
+  determinesIfTheLearnerHasTheNecessarySkillsForTheJob: number;
+  determinesIfTheLearnerHasTheJobRequirements: number;
+  considersTheJobInRelationToTheLearnerInterests: number;
+  analyzesIfTheJobIsCloseEnoughToTheResidence: number;
+  researchesJobOpenings: number;
+  researchesACompany: number;
+  createsAResume: number;
+  obtainsLettersOfReference: number;
+  savesCopiesOfOldJobsForFutureJobApplications: number;
+  fillsOutAnEmploymentApplicationForm: number;
+  writesACoverLetter: number;
+  createsACredentialPackage: number;
+  identifiesResourcesInTheCommunityThatCanHelpWithTheJobSearch: number;
+}
+
+interface InterviewPreparationAndExecutionSkills {
+  schedulingAnInterview: number;
+  usesTheCalendarToRememberScheduledInterviews: number;
+  securesTransportationToTheInterview: number;
+  preparesAListOfLikelyInterviewQuestions: number;
+  requestsTheInstructorToAttendTheInterview: number;
+  preparesAListOfQuestionsToAskTheEmployer: number;
+  preparesToTalkAboutOwnStrengthsInRelationToTheJob: number;
+  maintainsAppropriateHygieneForTheInterview: number;
+  answersVariousQuestionsDuringAMockInterview: number;
+  demonstratesInterpersonalSkillsDuringAMockInterview: number;
+  dressesProfessionally: number;
+  describesARelevantExperience: number;
+  asksAppropriateQuestions: number;
+  answersQuestionsInTheInterview: number;
+  usesACommunicationDevice: number; // Para confirmar detalhes, etc.
+  paysAttentionDuringTheInterview: number;
+  takesNotesDuringTheInterview: number;
+  determinesTheBenefitsOfThePositionDuringTheInterview: number;
+  talksAboutConcernsWithPreviousJobsAppropriately: number;
+  demonstratesInterpersonalSkillsDuringTheInterview: number;
+  preparesAThankYouNote: number;
+  respondsToNegotiableTermsOfThePosition: number;
+}
+
+interface WorkplaceSkills {
+  followsSimpleVerbalInstructions: number;
+  followsWrittenInstructions: number;
+  paysAttentionDuringTraining: number;
+  confirmsInformation: number;
+  learnsNewSkillsQuickly: number;
+  focusesOnSpecificTasks: number;
+  performsTasksThatAreNotEnjoyable: number;
+  completesAllTasksDuringTheShift: number;
+  finishesShiftTasks: number;
+  confirmsInformationAgain: number; // Repetição intencional, pode indicar necessidade de reforço
+  makesTransportationArrangements: number;
+  demonstratesConsistentAttendance: number;
+  demonstratesPunctualityOnArrivalAndDeparture: number;
+  notifiesTheCompanyWhenAbsentingFromWorkOrArrivingLate: number;
+  remainsCalmAtWork: number;
+  demonstratesGoodBehaviorInTheWorkplace: number;
+  demonstratesIntegrity: number;
+  seeksNewTasks: number;
+  tasksPlanning: number;
+  maintainsProcessSpeed: number;
+  solvesProblemsThatMayArise: number;
+  wearsAppropriateClothingForWork: number;
+  maintainsPersonalHygieneAtWork: number;
+  staysNeatWhilePerformingTasks: number;
+  adjustsClothingWhenChangingTasks: number;
+  keepsTheWorkAreaCleanAndTidy: number;
+  storageOfPersonalItems: number;
+  needsALocationToDealWithEmotionalIssues: number;
+  handlesMinorInjuries: number;
+  takesCareOfMedications: number;
+  notifiesSupervisorOfAccidents: number;
+  takesResponsibilityForMistakes: number;
+  wearsAnIdentificationBadge: number;
+  fillsOutForms: number;
+  usesAClock: number;
+  asksForABreak: number;
+  viewsThePayslip: number;
+  greetsACoworker: number;
+  handshake: number;
+  maintainsEyeContactWhenSpeaking: number;
+  interactsWithOtherPeopleCasually: number;
+  breaks: number;
+  breakEtiquette: number;
+  respectsPersonalSpace: number;
+}
+
+interface WorkplaceInteractionSkills {
+  identifiesTheSupervisor: number;
+  understandsTheHierarchy: number;
+  followsDirectOrdersFromSupervisor: number;
+  describesTheRoleOfCoworkers: number;
+  usesAppropriateConversationTopicsForWorkplace: number;
+  describesTheWorkCulture: number;
+  demonstratesAbilityToEngageInSmallTalkWithCoworkersAndBoss: number;
+  knowsTheAppropriateTimeToTalkAboutOtherPeopleBehavior: number;
+  avoidsDramaInTheWorkplace: number;
+  demonstratesAssertivenessWhenAppropriate: number;
+  respondsAppropriatelyToBullying: number;
+  removesOneselfFromConfrontationalSituations: number;
+  receivesSuggestionsAndFeedback: number;
+  offersSuggestions: number;
+  usesStrategiesForConflictResolution: number;
+}
+
+interface WorkplaceSafetySkills {
+  identifiesSignsOfDanger: number;
+  avoidsWetAreas: number;
+  avoidsHazardsRelatedToClutter: number;
+  usesStairsAndSteps: number;
+  avoidsHazardsRelatedToFallingFromHeights: number;
+  avoidsSituationsInvolvingFire: number;
+  avoidsHazardsRelatedToWorkingInTheCold: number; // Repetição corrigida
+  avoidsHazardsRelatedToBrokenGlass: number;
+  avoidsHazardsInvolvingToxicSubstances: number;
+  wearsAMask: number;
+  usesARespirator: number;
+  usesGasolineSafely: number;
+  avoidsHazardsRelatedToGas: number;
+  placesObjectsInASafeLocationToPreventFalling: number;
+  avoidsHazardsRelatedToElectricity: number;
+  avoidsHazardsRelatedToMovingVehiclesOrEquipment: number;
+  avoidsHazardsRelatedToMachinery: number;
+  avoidsHazardsRelatedToPlantsAndAnimals: number; // Repetição corrigida
+  wearsGloves: number;
+  wearsEarProtection: number;
+  wearsEyeProtection: number;
+  respondsAppropriatelyToNaturalDisasters: number;
+  respondsAppropriatelyToMedicalEmergencies: number;
+  respondsAppropriatelyToRobberies: number;
+  standsForLongPeriodsInMostJobs: number;
+  walksSufficientDistances: number;
+  maneuversAWheelchair: number;
+  demonstratesGrossMotorSkills: number;
+  demonstratesFineMotorSkills: number;
+  liftsHeavyObjectsCorrectly: number;
+  sortsItems: number;
+  usesACountingTray: number;
+  countsAndSeparatesItemsIntoBags: number;
+}
+
+interface MaterialHandlingAndManualTasksSkills {
+  carriesBoxes: number;
+  handlesItemsWithCare: number;
+  handlesHazardousItems: number;
+  usesTwistTies: number;
+  reducesItems: number; // Pode se referir a compactar, diminuir volume, etc.
+  foldsPaper: number;
+  makesPaperScraps: number;
+  makesAssemblies: number;
+  placesItemsOnTheAssemblyLine: number;
+}
+
+interface CleaningAndMaintenanceSkills {
+  cleansSinks: number;
+  cleansRoofs: number;
+  cleansWindowsAndMirrors: number;
+  cleansWalls: number;
+  cleansToilets: number;
+  cleansBathroomsUsingABrush: number;
+  dusts: number;
+  arrangesObjects: number;
+  cleansAWhiteboard: number;
+  cleansBathtubs: number;
+  removesBedding: number;
+  putsOnPillowcases: number;
+  putsOnSheets: number;
+  makesTheBed: number;
+  cleansUsingACloth: number;
+  cleansTheFloor: number; // Repetição mantida para enfatizar diferentes métodos
+  cleansTheCarpet: number;
+  cleansRugs: number; // Repetição mantida para diferenciar de carpetes fixos
+  usesAHandheldVacuum: number;
+  usesASwiffer: number;
+  waxesTheFloor: number;
+  recognizesWhenThingsNeedCleaning: number;
+  usesCleaningCloths: number;
+  identifiesAppropriateCleaningProductsForEachTask: number;
+  followsTheCleaningRoutine: number;
+  separatesTrashForRecycling: number;
+  emptiesTheTrash: number;
+  changesTrashBags: number;
+  changesToiletPaperRollsInBathrooms: number;
+  disposesOfHazardousOrInfectiousWaste: number;
+  changesLightBulbs: number;
+  correctUseOfLightBulbs: number;
+  stopsBathroomFlooding: number;
+  cleansDrains: number;
+  setsUpRoomsForMeetings: number;
+  cleansUpRoomsAfterEvents: number;
+  adjustsTheAirConditionerOrHeater: number;
+  locksAndUnlocksDoors: number;
+  operatesTheSecuritySystem: number;
+  worksSafelyNearSensitiveEquipment: number;
+  respectsOtherPeoplesItemsAtWork: number;
+  identifiesAndReportsImmediateRepairProblems: number;
+  identifiesAndReportsProblemsThatRequireSpecializedHelp: number;
+  usesCautionSigns: number;
+  keepsDoorsClosedAfterCleaning: number;
+}
+
+interface LaundrySkills {
+  separatesClothesByColor: number;
+  usesTheWashingMachine: number;
+  putsClothesInTheDryer: number;
+  usesTheDryer: number;
+  emptiesTheDryer: number;
+  hangsClothesToDry: number;
+  separatesClothesForFolding: number;
+  foldsTowels: number;
+  foldsPairsOfSocks: number;
+  foldsPants: number;
+  foldsTShirts: number;
+  foldsShirts: number;
+  foldsSheets: number;
+  identifiesDamagedClothes: number;
+}
+
+interface RetailSkills {
+  identifiesProducts: number;
+  labelsProducts: number;
+  placesBoxesInStock: number;
+  stacksBoxes: number;
+  putsClothesOnDisplay: number;
+  setsPrices: number;
+  usesALabelMakerToPrintPriceTags: number;
+  stocksShelvesWithCansBottlesBoxesEtc: number;
+  discriminatesBySizes: number;
+  hangsClothes: number;
+  foldsAndStacksClothes: number;
+  arrangesClothesNeatly: number;
+  dressesTheMannequin: number;
+  gathersShoppingCarts: number;
+  arrangesTheDisplay: number;
+  arrangesItemsFromVariousParts: number;
+  greetsCustomers: number;
+  interactsAppropriatelyWithCustomers: number;
+  sellsProducts: number;
+  checksStockForSpecificMerchandise: number;
+  usesACashRegister: number;
+  bagsItems: number;
+}
+
+interface RecreationAndChildcareSkills {
+  cleansToysAndEquipment: number; // Repetição corrigida
+  servesFoodAndDrinks: number;
+  conductsAVarietyOfRoomActivities: number;
+  playsGames: number;
+  monitorsRecreationalSpaces: number;
+}
+
+interface OfficeSkills {
+  makesCopies: number;
+  maintainsTheCopier: number;
+  arrangesPaper: number;
+  staplesPaper: number;
+  usesA3HolePunch: number;
+  usesA3RingBinder: number;
+  usesBinders: number;
+  usesPaperClips: number;
+  usesThumbtacks: number;
+  hangsPaperUsingMagnets: number;
+  usesPencilSharpeners: number;
+  emptiesThePencilSharpener: number;
+  shredsPaper: number;
+  cleansTheShredder: number;
+  usesAPaperCutter: number;
+  usesALaminatingMachine: number;
+  usesTheFaxMachine: number;
+  opensLettersAndPackages: number;
+  storesEnvelopes: number;
+  placesAddressLabelsOnEnvelopes: number;
+  addressesEnvelopes: number;
+  distributesMail: number;
+  packagesProductsInsideContainers: number;
+  determinesIfLettersNeedMoreThanOneStamp: number;
+  usesPostageMeters: number;
+  usesShippingAndTrackingOptions: number;
+  signsForDeliveries: number;
+  sortsByNumericalOrder: number;
+  sortsAlphabetically: number;
+  labelsFileFolders: number;
+  createsNewFileFolderStorageLocations: number;
+  usesFilingCabinets: number;
+  counts100Items: number;
+  countsInventory: number;
+  ordersSupplies: number;
+}
+
+interface ComputerSkills {
+  turnsOnTheComputer: number;
+  usesTheMouse: number;
+  usesTheCursorWithFingers: number; // (Touchpad ou tela sensível ao toque)
+  logsInAndOutOfAComputer: number;
+  waitsWhenPrompted: number;
+  beginsSavingNewDocuments: number;
+  savesARevisedCopyOfADocumentAsANewDocument: number;
+  formatsADocument: number;
+  changesFonts: number; // Repetição corrigida
+  usesSpellCheck: number;
+  createsAndUsesFiles: number;
+  schedulesEventsOnTheComputer: number;
+  opensAComputerProgram: number;
+  usesADVDDrive: number;
+  identifiesWhichProgramsNeedToBeOpenedForCertainTasks: number;
+  printsVariousContent: number;
+  usesACD: number;
+  typesUsingTheCorrectFingerPlacement: number;
+  usesSearchEngines: number;
+  usesUsernamesAndSecurePasswords: number;
+  backsUpComputerData: number;
+  usesAPortableDrive: number;
+  searchesForFiles: number;
+  usesALaptop: number;
+  ensuresComputerSecurity: number;
+  monitorsTheLaptopBattery: number;
+  usesASpreadsheetProgram: number;
+  completesComputerFormulas: number;
+  sendsAndReceivesEmails: number;
+  usesOnlineMapsForDirections: number;
+}
+
+interface FoodServiceSkills {
+  cleansTablesAndChairs: number;
+  arrangesTables: number;
+  cleansTablesCountersAndChairs: number;
+  clearsDishesToBeWashed: number;
+  placesDishesInTheDishwasher: number;
+  operatesTheDishwasher: number;
+  removesDishesFromTheDishwasher: number;
+  washesDishesManually: number;
+  clearsUsedDishesFromTables: number;
+  separatesCutlery: number;
+  rollsCutleryInNapkins: number;
+  replenishesNapkins: number;
+  replenishesCondimentPackets: number;
+  replenishesCondimentsSaltAndPepperInContainers: number;
+  placesPlatesAndCutleryForSelfService: number;
+  setsTheTable: number;
+  replenishesColdDrinks: number;
+  replenishesHotDrinks: number;
+  servesMeals: number;
+  wrapsFoodBeforeStoring: number;
+  restocksFoodInventory: number;
+  organizesFoodAndSupplies: number;
+  replenishesItemsOnTheBuffet: number;
+  storesFood: number;
+  determinesIfFoodIsUnsafeBySmellAppearanceAndTime: number; // Repetição corrigida
+  takesCustomerOrders: number;
+  clearsDirtyDishesFromTables: number;
+  asksCustomersIfTheyNeedAnything: number;
+  fulfillsCustomerRequests: number; // Repetição corrigida
+  resolvesCustomerComplaints: number;
+}
+
+interface FoodPreparationSkills {
+  usesUtensils: number;
+  usesKnives: number;
+  knowsMeasurementAbbreviations: number;
+  measuresWeights: number;
+  measuresLiquids: number;
+  measuresVolumes: number;
+  knowsMeasurementAbbreviations2: number; // Redundância mantida para clareza
+  measuresSolidVolumes: number;
+  knowsEquivalentMeasurements: number;
+  usesACanOpener: number;
+  wearsAHairnet: number;
+  wearsAnApron: number;
+  washesHandsWhenNecessary: number;
+  coversCoughsToPreventSpreadingGerms: number;
+  handlesFoodHygienically: number;
+  makesSalads: number;
+  makesCoffee: number;
+  completesVariousFoodPreparationActivities: number;
+  preparesSimpleItems: number;
+  placesDishesInOrOutOfTheOven: number;
+  knowsWhenFoodIsReady: number;
+  servesAppropriatePortionsOnPlatesAndDishes: number;
+  servesFoodInADecorativeAndDistributedManner: number;
+}
+
+interface WarehouseSkills {
+  identifiesContentsInBoxes: number;
+  locatesWarehouseInventory: number;
+  opensBoxes: number;
+  picksUpItems: number;
+  placesItemsInBoxes: number;
+  placesBoxesOnPallets: number;
+  arrangesBoxes: number;
+  disassemblesBoxes: number;
+  verifiesReceivingInventory: number;
+  preventsInventoryDamageOrLoss: number; // Repetição corrigida
+  usesAHandTruck: number; // "p r a t e l e i r a manual" traduzido como carrinho de mão.
+  usesAForklift: number;
+}
+
+interface ManualLaborSkills {
+  usesNails: number;
+  advancedHammerUse: number;
+  identifiesDifferentTypesOfNails: number;
+  usesAScrewdriver: number;
+  advancedScrewdriverTasks: number;
+  choosesTheCorrectScrewsForTheTask: number;
+  usesAHandSaw: number;
+  usesSandpaperToSandWood: number;
+  usesClamps: number;
+  usesWrenches: number;
+  usesSocketWrenches: number;
+  identifiesAndChoosesTheCorrectNutsBoltsAndWashers: number;
+  usesATapeMeasure: number;
+  usesChalkLines: number;
+  usesAStudFinder: number;
+  usesALevel: number;
+  usesACaulkingGun: number;
+  usesTwoToolsSimultaneouslyToCompleteATask: number;
+  usesANailGun: number;
+  operatesASander: number;
+  sandsWood: number;
+  usesAPowerSaw: number;
+  cutsUsingASaw: number;
+  selectsSawAndBladeForTheTask: number;
+  usesADrill: number;
+  buildsSmallStructuresUsingVariousTools: number;
+  carriesToolsAndEquipment: number;
+  carriesToolsAndEquipmentDuringUse: number;
+  knowsTheNamesOfTheWorkRelatedToolsAndWhichFitTheFunction: number;
+  knowsFunctionsOfWorkTools: number;
+}
+
+interface MaintenanceSkills {
+  deliversItemsToPeopleAtTheWorkplace: number;
+  movesItemsWithinTheWorkplace: number;
+  movesAndChangesElectricalOutlets: number;
+  coversEdges: number;
+  usesCloths: number; // Assumindo que se refere a panos de limpeza/proteção
+  preparesForPainting: number;
+  paintsUsingABrush: number;
+  paintsWithARoller: number;
+  cleansRollerAndBrush: number;
+  cutsPvcAndCopperPipes: number;
+  assemblesPipes: number;
+  installsOrReplacesWaterCoolers: number;
+  removesAndReplacesAPTrap: number;
+  installsOrReplacesFaucets: number;
+  makesSmallRepairsToTheToilet: number;
+  turnsOffTheMainElectricity: number;
+  resetsACircuitBreaker: number;
+  joinsElectricalWires: number;
+  removesAndConnectsWires: number;
+  attachesWiresToOutlets: number;
+  testsElectricalCurrent: number;
+  identifiesAndSelectsCablesForSpecificTasks: number;
+  laysComputerCables: number;
+  cutsDrywall: number;
+  installsDrywall: number;
+  tapesDrywall: number;
+  dampensAndDriesTheWall: number;
+  remainsSafeOnTheRoof: number;
+  removesExistingRoofTiles: number;
+  usesRoofingUnderlayment: number;
+  installsRoofTiles: number;
+  cutsCarpetAndUpholstery: number;
+  appliesTackStrips: number;
+  nailsTheCarpetToTheSubfloor: number;
+  laysCarpet: number;
+  laysCarpetOnMarkedLines: number;
+  checksAndAdjustsTackStrips: number;
+  changesTires: number;
+  performsRoutineAutomotiveCleaningTasks: number;
+  performsRoutineAutomotiveMaintenanceTasks: number;
+}
+
+interface GardeningSkills {
+  usesHoses: number;
+  watersPlants: number;
+  plants: number;
+  removesDeadPlantsAndDisposesOfThem: number;
+  appliesMulch: number;
+  usesShovels: number;
+  removesLeaves: number;
+  bagsLeaves: number;
+  prunesShrubsAndTrees: number;
+  appliesFertilizer: number;
+  appliesChemicalsToVegetation: number;
+  mowsTheLawn: number;
+  usesGardeningShears: number;
+  usesATrimmer: number;
+  usesABlower: number;
+}
