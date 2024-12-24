f35483f4097c6f1859bf6d4defda068ff2fdc7f2
LucasVannucchi <lucasvannucchi16@gmail.com>
2024-12-24 01:41:40 -0300

    refactoring the code "Type" incomeTable.ts

--- src/features/income-table/types/incomeTable.ts
+++ src/features/income-table/types/incomeTable.ts
@@ -1,131 +1,34 @@
 export interface IIncomeTable{
     
     incomeTablesPartOne:{
-        recognizesAndIdentifiesLetters:[{
-            optionYes: string;
-            optionNo: string;
-            optinoInDevelopment: string;
-        }];
-        recognizesAndIdentifiesNumbers:[{
-            optionYes: string;
-            optionNo: string;
-            optinoInDevelopment: string;
-        }];
-        directedWriting:[{
-            optionYes: string;
-            optionNo: string;
-            optinoInDevelopment: string;
-        }];
-        spontaneousWriting:[{
-            optionYes: string;
-            optionNo: string;
-            optinoInDevelopment: string;
-        }];
-        readingAndWriting:[{
-            optionYes: string;
-            optionNo: string;
-            optinoInDevelopment: string;
-        }];
-        readingAndUnderstanding:[{
-            optionYes: string;
-            optionNo: string;
-            optinoInDevelopment: string;
-        }];
-        textProductionRegistration:[{
-            optionYes: string;
-            optionNo: string;
-            optinoInDevelopment: string;
-        }];
-        textualStructuring:[{
-            optionYes: string;
-            optionNo: string;
-            optinoInDevelopment: string;
-        }];
-        oralTextProduction:[{
-            optionYes: string;
-            optionNo: string;
-            optinoInDevelopment: string;
-        }];
-        largerNumbers:[{
-            optionYes: string;
-            optionNo: string;
-            optinoInDevelopment: string;
-        }];
-        troubleshooting:[{
-            optionYes: string;
-            optionNo: string;
-            optinoInDevelopment: string;
-        }];
-
+        recognizesAndIdentifiesLetters: string;
+        recognizesAndIdentifiesNumbers: string;
+        directedWriting: string;
+        spontaneousWriting: string;
+        readingAndWriting: string;
+        readingAndUnderstanding: string;
+        textProductionRegistration: string;
+        textualStructuring: string;
+        oralTextProduction: string;
+        largerNumbers: string;
+        troubleshooting: string;
     };
 
 
 
     incomeTablesPartTwo:{
-        bodyScheme:[{
-            optionYes: string;
-            optionNo: string;
-            optinoInDevelopment: string;
-        }];
-        sidePointing:[{
-            optionYes: string;
-            optionNo: string;
-            optinoInDevelopment: string;
-        }];
-        position:[{
-            optionYes: string;
-            optionNo: string;
-            optinoInDevelopment: string;
-        }];
-        direction:[{
-            optionYes: string;
-            optionNo: string;
-            optinoInDevelopment: string;
-        }];
-        space:[{
-            optionYes: string;
-            optionNo: string;
-            optinoInDevelopment: string;
-        }];
-        size:[{
-            optionYes: string;
-            optionNo: string;
-            optinoInDevelopment: string;
-        }];
-        quantity:[{
-            optionYes: string;
-            optionNo: string;
-            optinoInDevelopment: string;
-        }];
-        shape:[{
-            optionYes: string;
-            optionNo: string;
-            optinoInDevelopment: string;
-        }];
-        visualDiscrimination:[{
-            optionYes: string;
-            optionNo: string;
-            optinoInDevelopment: string;
-        }];
-        hearingDiscrimination:[{
-            optionYes: string;
-            optionNo: string;
-            optinoInDevelopment: string;
-        }];
-        verbalizationOfWords:[{
-            optionYes: string;
-            optionNo: string;
-            optinoInDevelopment: string;
-        }];
-        synthesisAnalysis:[{
-            optionYes: string;
-            optionNo: string;
-            optinoInDevelopment: string;
-        }];
-        fineMotorCoordination:[{
-            optionYes: string;
-            optionNo: string;
-            optinoInDevelopment: string;
-        }];
+        bodyScheme: string;
+        sidePointing: string;
+        position: string;
+        direction: string;
+        space: string;
+        size: string;
+        quantity: string;
+        shape: string;
+        visualDiscrimination: string;
+        hearingDiscrimination: string;
+        verbalizationOfWords: string;
+        synthesisAnalysis: string;
+        fineMotorCoordination: string;
     }
 };
