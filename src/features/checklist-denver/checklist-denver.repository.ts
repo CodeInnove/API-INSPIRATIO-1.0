c10673ba0cd5c83b31f900953703012f466b450a
Juan Guillherme <guilhermejuan729@gmail.com>
2025-01-09 15:42:26 -0300

    feat(checklist denver): adicionando teste de criação para graficos

--- src/features/checklist-denver/checklist-denver.repository.ts
+++ src/features/checklist-denver/checklist-denver.repository.ts
@@ -31,3 +31,69 @@ export class ChecklistDenverRepository {
     return await this.denverModel.findByIdAndDelete(id).exec();
   }
 }
+
+
+/*async createGraph(createGraphDto: CreateGraphDto): Promise<Graph> {
+    const { level, ability, answers } = createGraphDto;
+
+    const acquiredCount = Object.values(answers).filter(answer => answer === 'adquirido').length;
+    const notAcquiredCount = Object.values(answers).filter(answer => answer === 'não adquirido').length;
+    const partialCount = Object.values(answers).filter(answer => answer === 'parcial').length;
+
+    const totalAnswers = Object.values(answers).length;
+
+    const acquiredPercentage = totalAnswers > 0 ? (acquiredCount / totalAnswers) * 100 : 0;
+    const notAcquiredPercentage = totalAnswers > 0 ? (notAcquiredCount / totalAnswers) * 100 : 0;
+    const partialPercentage = totalAnswers > 0 ? (partialCount / totalAnswers) * 100 : 0;
+
+    const graph = this.graphRepository.create({
+        level,
+        ability,
+        answers,
+        acquiredCount,
+        notAcquiredCount,
+        partialCount,
+        acquiredPercentage,
+        notAcquiredPercentage,
+        partialPercentage
+    });
+    return this.graphRepository.save(graph);
+  }
+
+  async findGraphByLevelAndAbility(level: Level, ability: Ability): Promise<Graph | undefined> {
+    return this.graphRepository.findOne({ where: { level, ability } });
+  }
+
+  async updateGraph(id: number, updateGraphDto: UpdateGraphDto): Promise<Graph | undefined> {
+        const existingGraph = await this.graphRepository.findOneBy({ id });
+        if (!existingGraph) {
+          return undefined;
+        }
+
+        const { level, ability, answers } = updateGraphDto;
+    
+        const acquiredCount = answers ? Object.values(answers).filter(answer => answer === 'adquirido').length : existingGraph.acquiredCount;
+        const notAcquiredCount = answers ? Object.values(answers).filter(answer => answer === 'não adquirido').length : existingGraph.notAcquiredCount;
+        const partialCount = answers ? Object.values(answers).filter(answer => answer === 'parcial').length : existingGraph.partialCount;
+    
+        const totalAnswers = answers ? Object.values(answers).length : Object.values(existingGraph.answers).length;
+    
+        const acquiredPercentage = totalAnswers > 0 ? (acquiredCount / totalAnswers) * 100 : 0;
+        const notAcquiredPercentage = totalAnswers > 0 ? (notAcquiredCount / totalAnswers) * 100 : 0;
+        const partialPercentage = totalAnswers > 0 ? (partialCount / totalAnswers) * 100 : 0;
+    
+        const updatedGraph = {
+            ...existingGraph,
+            level: level !== undefined ? level : existingGraph.level,
+            ability: ability !== undefined ? ability : existingGraph.ability,
+            answers: answers !== undefined ? answers : existingGraph.answers,
+            acquiredCount,
+            notAcquiredCount,
+            partialCount,
+            acquiredPercentage,
+            notAcquiredPercentage,
+            partialPercentage
+        }
+
+        return this.graphRepository.save(updatedGraph);
+  }*/
