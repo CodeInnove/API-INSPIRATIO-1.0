a20e07ee4311ebb8a4f8c51dea4a6788b786efb1
LucasVannucchi <lucasvannucchi16@gmail.com>
2025-01-17 22:55:26 -0300

    alterando APDG

--- src/features/psychopedagogical-anamnesis/dto/psychopedagogicalAnamnesisCreate.dto.ts
+++ src/features/psychopedagogical-anamnesis/dto/psychopedagogicalAnamnesisCreate.dto.ts
@@ -1,430 +1,418 @@
 import { ApiProperty } from '@nestjs/swagger';
-import { IsBoolean, IsNotEmpty, IsNumber, IsOptional, IsString, ValidateNested } from 'class-validator';
+import {
+  IsBoolean,
+  IsNotEmpty,
+  IsNumber,
+  IsOptional,
+  IsString,
+  ValidateNested,
+} from 'class-validator';
 import { Type } from 'class-transformer';
 
-
-
 class FamilyLearningHistoryDto {
-    @ApiProperty()
-    @IsString()
-    @IsOptional()
-    fatherName: string;
+  @ApiProperty()
+  @IsString()
+  @IsOptional()
+  fatherName: string;
 
-    @ApiProperty()
-    @IsNumber()
-    @IsOptional()
-    ageFather: number;
+  @ApiProperty()
+  @IsNumber()
+  @IsOptional()
+  ageFather: number;
 
-    @ApiProperty()
-    @IsString()
-    @IsOptional()
-    schoolingFather: string;
+  @ApiProperty()
+  @IsString()
+  @IsOptional()
+  schoolingFather: string;
 
-    @ApiProperty()
-    @IsString()
-    @IsOptional()
-    difficultyFather: string;
+  @ApiProperty()
+  @IsString()
+  @IsOptional()
+  difficultyFather: string;
 
-    @ApiProperty()
-    @IsString()
-    @IsOptional()
-    motherName: string;
+  @ApiProperty()
+  @IsString()
+  @IsOptional()
+  motherName: string;
 
-    @ApiProperty()
-    @IsNumber()
-    @IsOptional()
-    ageMother: number;
+  @ApiProperty()
+  @IsNumber()
+  @IsOptional()
+  ageMother: number;
 
-    @ApiProperty()
-    @IsString()
-    @IsOptional()
-    schoolingMother: string;
+  @ApiProperty()
+  @IsString()
+  @IsOptional()
+  schoolingMother: string;
 
-    @ApiProperty()
-    @IsString()
-    @IsOptional()
-    difficultyMother: string;
+  @ApiProperty()
+  @IsString()
+  @IsOptional()
+  difficultyMother: string;
 
-    @ApiProperty()
-    @IsString()
-    @IsOptional()
-    nameBrotherOne: string;
+  @ApiProperty()
+  @IsString()
+  @IsOptional()
+  nameBrotherOne: string;
 
-    @ApiProperty()
-    @IsNumber()
-    @IsOptional()
-    ageBrotherOne: number;
+  @ApiProperty()
+  @IsNumber()
+  @IsOptional()
+  ageBrotherOne: number;
 
-    @ApiProperty()
-    @IsString()
-    @IsOptional()
-    schoolingBrotherOne: string;
+  @ApiProperty()
+  @IsString()
+  @IsOptional()
+  schoolingBrotherOne: string;
 
-    @ApiProperty()
-    @IsString()
-    @IsOptional()
-    difficultyBrotherOne: string;
+  @ApiProperty()
+  @IsString()
+  @IsOptional()
+  difficultyBrotherOne: string;
 
-    @ApiProperty()
-    @IsString()
-    @IsOptional()
-    nameBrotherTwo: string;
+  @ApiProperty()
+  @IsString()
+  @IsOptional()
+  nameBrotherTwo: string;
 
-    @ApiProperty()
-    @IsNumber()
-    @IsOptional()
-    ageBrotherTwo: number;
+  @ApiProperty()
+  @IsNumber()
+  @IsOptional()
+  ageBrotherTwo: number;
 
-    @ApiProperty()
-    @IsString()
-    @IsOptional()
-    schoolingBrotherTwo: string;
+  @ApiProperty()
+  @IsString()
+  @IsOptional()
+  schoolingBrotherTwo: string;
 
-    @ApiProperty()
-    @IsString()
-    @IsOptional()
-    difficultyBrotherTwo: string;
+  @ApiProperty()
+  @IsString()
+  @IsOptional()
+  difficultyBrotherTwo: string;
 
-    @ApiProperty()
-    @IsString()
-    @IsOptional()
-    nameBrotherThree: string;
+  @ApiProperty()
+  @IsString()
+  @IsOptional()
+  nameBrotherThree: string;
 
-    @ApiProperty()
-    @IsNumber()
-    @IsOptional()
-    ageBrotherThree: number;
+  @ApiProperty()
+  @IsNumber()
+  @IsOptional()
+  ageBrotherThree: number;
 
-    @ApiProperty()
-    @IsString()
-    @IsOptional()
-    schoolingBrotherThree: string;
+  @ApiProperty()
+  @IsString()
+  @IsOptional()
+  schoolingBrotherThree: string;
 
-    @ApiProperty()
-    @IsString()
-    @IsOptional()
-    difficultyBrotherThree: string;
+  @ApiProperty()
+  @IsString()
+  @IsOptional()
+  difficultyBrotherThree: string;
 }
 
+class ObservationDto {
+  @ApiProperty()
+  @IsNumber()
+  @IsOptional()
+  bedtime: string;
 
-class ObservationDto{
-    @ApiProperty()
-    @IsNumber()
-    @IsOptional()
-    bedtime: number;
+  @ApiProperty()
+  @IsNumber()
+  @IsOptional()
+  wakeUpTime: string;
 
-    @ApiProperty()
-    @IsNumber()
-    @IsOptional()
-    wakeUpTime: number;
+  @ApiProperty()
+  @IsString()
+  @IsOptional()
+  sportsActivitiesHeLikesTheMost: string;
 
-    @ApiProperty()
-    @IsString()
-    @IsOptional()
-    sportsActivitiesHeLikesTheMost: string;
+  @ApiProperty()
+  @IsString()
+  @IsOptional()
+  externalServiceActivities: string;
 
-    @ApiProperty()
-    @IsString()
-    @IsOptional()
-    externalServiceActivities: string;
+  @ApiProperty()
+  @IsBoolean()
+  @IsOptional()
+  doYouUseAdaptedActivitiesAtSchool: string;
 
-    @ApiProperty()
-    @IsBoolean()
-    @IsOptional()
-    doYouUseAdaptedActivitiesAtSchool: boolean;
+  @ApiProperty()
+  @IsString()
+  @IsOptional()
+  whatActivitiesAreAdapted: string;
 
-    @ApiProperty()
-    @IsString()
-    @IsOptional()
-    whatActivitiesAreAdapted: string;
+  @ApiProperty()
+  @IsBoolean()
+  @IsOptional()
+  doYouHaveDifficultyInSocialInteraction: string;
 
-    @ApiProperty()
-    @IsBoolean()
-    @IsOptional()
-    doYouHaveDifficultyInSocialInteraction: boolean;
+  @ApiProperty()
+  @IsBoolean()
+  @IsOptional()
+  whatDifficultyInSocialInteraction: string;
 
-    @ApiProperty()
-    @IsBoolean()
-    @IsOptional()
-    whatDifficultyInSocialInteraction: boolean;
+  @ApiProperty()
+  @IsBoolean()
+  @IsOptional()
+  resistanceToRoutineChange: string;
 
+  @ApiProperty()
+  @IsString()
+  @IsOptional()
+  whatResistanceToRoutineChange: string;
 
-    @ApiProperty()
-    @IsBoolean()
-    @IsOptional()
-    resistanceToRoutineChange: boolean;
+  @ApiProperty()
+  @IsBoolean()
+  @IsOptional()
+  eyeContact: string;
 
-    @ApiProperty()
-    @IsString()
-    @IsOptional()
-    whatResistanceToRoutineChange: string;
+  @ApiProperty()
+  @IsString()
+  @IsOptional()
+  whatEyeContact: string;
 
+  @ApiProperty()
+  @IsBoolean()
+  @IsOptional()
+  interestInLearning: string;
 
-    @ApiProperty()
-    @IsBoolean()
-    @IsOptional()
-    eyeContact: boolean;
+  @ApiProperty()
+  @IsString()
+  @IsOptional()
+  whatInterestInLearning: string;
 
-    @ApiProperty()
-    @IsString()
-    @IsOptional()
-    whatEyeContact: string;
+  @ApiProperty()
+  @IsBoolean()
+  @IsOptional()
+  hyperactivity: string;
 
+  @ApiProperty()
+  @IsString()
+  @IsOptional()
+  whatHyperactivity: string;
 
-    @ApiProperty()
-    @IsBoolean()
-    @IsOptional()
-    interestInLearning: boolean;
+  @ApiProperty()
+  @IsBoolean()
+  @IsOptional()
+  inattention: string;
 
-    @ApiProperty()
-    @IsString()
-    @IsOptional()
-    whatInterestInLearning: string;
+  @ApiProperty()
+  @IsString()
+  @IsOptional()
+  whatInattention: string;
 
-    @ApiProperty()
-    @IsBoolean()
-    @IsOptional()
-    hyperactivity: boolean;
+  @ApiProperty()
+  @IsBoolean()
+  @IsOptional()
+  areYouLiterate: string;
 
-    @ApiProperty()
-    @IsString()
-    @IsOptional()
-    whatHyperactivity: string;
+  @ApiProperty()
+  @IsString()
+  @IsOptional()
+  whatAreYouLiterate: string;
 
+  @ApiProperty()
+  @IsBoolean()
+  @IsOptional()
+  changeInLanguageAndCommunication: string;
 
-    @ApiProperty()
-    @IsBoolean()
-    @IsOptional()
-    inattention: boolean;
+  @ApiProperty()
+  @IsString()
+  @IsOptional()
+  whatLanguageAndCommunication: string;
 
+  @ApiProperty()
+  @IsBoolean()
+  @IsOptional()
+  inappropriateReactionToFrustration: string;
 
-    @ApiProperty()
-    @IsString()
-    @IsOptional()
-    whatInattention: string;
+  @ApiProperty()
+  @IsString()
+  @IsOptional()
+  whatReactionToFrustration: string;
 
+  @ApiProperty()
+  @IsBoolean()
+  @IsOptional()
+  crisisTypeCrisisTrigger: string;
 
-    @ApiProperty()
-    @IsBoolean()
-    @IsOptional()
-    areYouLiterate: boolean;
+  @ApiProperty()
+  @IsString()
+  @IsOptional()
+  whatTypeCrisisTrigger: string;
 
-    @ApiProperty()
-    @IsString()
-    @IsOptional()
-    whatAreYouLiterate: string;
+  @ApiProperty()
+  @IsBoolean()
+  @IsOptional()
+  stereotypy: string;
 
+  @ApiProperty()
+  @IsString()
+  @IsOptional()
+  whatStereotypy: string;
 
-    @ApiProperty()
-    @IsBoolean()
-    @IsOptional()
-    changeInLanguageAndCommunication: boolean;
+  @ApiProperty()
+  @IsBoolean()
+  @IsOptional()
+  medication: string;
 
-    @ApiProperty()
-    @IsString()
-    @IsOptional()
-    whatLanguageAndCommunication: string;
+  @ApiProperty()
+  @IsString()
+  @IsOptional()
+  whatMedication: string;
 
+  @ApiProperty()
+  @IsBoolean()
+  @IsOptional()
+  allergy: string;
 
-    @ApiProperty()
-    @IsBoolean()
-    @IsOptional()
-    inappropriateReactionToFrustration: boolean;
+  @ApiProperty()
+  @IsString()
+  @IsOptional()
+  whatAllergy: string;
 
-    @ApiProperty()
-    @IsString()
-    @IsOptional()
-    whatReactionToFrustration: string;
+  @ApiProperty()
+  @IsString()
+  @IsOptional()
+  Diagnosis: string;
 
+  @ApiProperty()
+  @IsString()
+  @IsOptional()
+  schoolFamilyComplaint: string;
 
-    @ApiProperty()
-    @IsBoolean()
-    @IsOptional()
-    crisisTypeCrisisTrigger: boolean;
-
-    @ApiProperty()
-    @IsString()
-    @IsOptional()
-    whatTypeCrisisTrigger: string;
-
-
-    @ApiProperty()
-    @IsBoolean()
-    @IsOptional()
-    stereotypy: boolean;
-
-    @ApiProperty()
-    @IsString()
-    @IsOptional()
-    whatStereotypy: string;
-
-
-    @ApiProperty()
-    @IsBoolean()
-    @IsOptional()
-    medication: boolean;
-
-    @ApiProperty()
-    @IsString()
-    @IsOptional()
-    whatMedication: string;
-
-
-    @ApiProperty()
-    @IsBoolean()
-    @IsOptional()
-    allergy: boolean;
-
-    @ApiProperty()
-    @IsString()
-    @IsOptional()
-    whatAllergy: string;
-
-
-    @ApiProperty()
-    @IsString()
-    @IsOptional()
-    Diagnosis: string;
-
-    @ApiProperty()
-    @IsString()
-    @IsOptional()
-    schoolFamilyComplaint: string;
-
-    @ApiProperty()
-    @IsString()
-    @IsOptional()
-    outher: string;
-
+  @ApiProperty()
+  @IsString()
+  @IsOptional()
+  outher: string;
 }
 
-class LearningAssessmentDto{
-    @ApiProperty()
-    @IsString()
-    @IsOptional()
-    readingLevel: string;
+class LearningAssessmentDto {
+  @ApiProperty()
+  @IsString()
+  @IsOptional()
+  readingLevel: string;
 
-    @ApiProperty()
-    @IsString()
-    @IsOptional()
-    textProduction: string;
+  @ApiProperty()
+  @IsString()
+  @IsOptional()
+  textProduction: string;
 
-    @ApiProperty()
-    @IsString()
-    @IsOptional()
-    preSyllabicWritingLevel: string;
-        
-    @ApiProperty()
-    @IsBoolean()
-    @IsOptional()
-    doYouRecognizeTheColors: boolean;                                                                                                         
+  @ApiProperty()
+  @IsString()
+  @IsOptional()
+  preSyllabicWritingLevel: string;
 
-    @ApiProperty()
-    @IsBoolean()
-    @IsOptional()
-    difficultyRecognizeTheColors: boolean;
+  @ApiProperty()
+  @IsBoolean()
+  @IsOptional()
+  doYouRecognizeTheColors: string;
 
+  @ApiProperty()
+  @IsBoolean()
+  @IsOptional()
+  difficultyRecognizeTheColors: string;
 
-    @ApiProperty()
-    @IsBoolean()
-    @IsOptional()
-    doYouHaveDifficultyTracingTheNumerals: boolean;
-    
-    @ApiProperty()
-    @IsBoolean()
-    @IsOptional()
-    difficultyRecognizingNumbers: boolean;
+  @ApiProperty()
+  @IsBoolean()
+  @IsOptional()
+  doYouHaveDifficultyTracingTheNumerals: string;
 
-    @ApiProperty()
-    @IsString()
-    @IsOptional()
-    doYouHaveAChangeWhenCarryingOutOperations: string;
+  @ApiProperty()
+  @IsBoolean()
+  @IsOptional()
+  difficultyRecognizingNumbers: string;
 
-    @ApiProperty()
-    @IsBoolean()
-    @IsOptional()
-    difficultyCarryingOutOperations: boolean;
+  @ApiProperty()
+  @IsString()
+  @IsOptional()
+  doYouHaveAChangeWhenCarryingOutOperations: string;
 
-    @ApiProperty()
-    @IsBoolean()
-    @IsOptional()
-    doesItSolveProblemSituations:  boolean;
+  @ApiProperty()
+  @IsBoolean()
+  @IsOptional()
+  difficultyCarryingOutOperations: string;
+
+  @ApiProperty()
+  @IsBoolean()
+  @IsOptional()
+  doesItSolveProblemSituations: string;
 }
 
 export class PsychopedagogicalAnamnesisCreateDto {
+  @ApiProperty()
+  @IsString()
+  @IsOptional()
+  patient: string;
 
-    @ApiProperty()
-    @IsString()
-    @IsOptional()
-    patient: string;
+  @ApiProperty()
+  @IsString()
+  @IsOptional()
+  dateOfApplication: string;
 
-    @ApiProperty()
-    @IsString()
-    @IsOptional()
-    dateOfApplication: string;
+  @ApiProperty()
+  @IsString()
+  @IsOptional()
+  dateBirth: string;
 
-    @ApiProperty()
-    @IsString()
-    @IsOptional()
-    dateBirth: string;
+  @ApiProperty()
+  @IsNumber()
+  @IsOptional()
+  age: number;
 
-    @ApiProperty()
-    @IsNumber()
-    @IsOptional()
-    age: number;
+  @ApiProperty()
+  @IsString()
+  @IsOptional()
+  teacherName: string;
 
-    @ApiProperty()
-    @IsString()
-    @IsOptional()
-    teacherName: string;
+  @ApiProperty()
+  @IsString()
+  @IsOptional()
+  supportProfessional: string;
 
-    @ApiProperty()
-    @IsString()
-    @IsOptional()
-    supportProfessional: string;
+  @ApiProperty()
+  @IsString()
+  @IsOptional()
+  schoolName: string;
 
-    @ApiProperty()
-    @IsString()
-    @IsOptional()
-    schoolName: string;
+  @ApiProperty()
+  @IsString()
+  @IsOptional()
+  schoolPeriod: string;
 
-    @ApiProperty()
-    @IsString()
-    @IsOptional()
-    schoolPeriod: string;
+  @ApiProperty()
+  @IsString()
+  @IsOptional()
+  schoolYear: string;
 
-    @ApiProperty()
-    @IsString()
-    @IsOptional()
-    schoolYear: string;
+  @ApiProperty()
+  @ValidateNested()
+  @Type(() => FamilyLearningHistoryDto)
+  familyLearningHistory: FamilyLearningHistoryDto;
 
-    @ApiProperty()
-    @ValidateNested()
-    @Type(() => FamilyLearningHistoryDto)
-    familyLearningHistory: FamilyLearningHistoryDto;
+  @ApiProperty()
+  @ValidateNested()
+  @Type(() => ObservationDto)
+  observation: ObservationDto;
 
-    @ApiProperty()
-    @ValidateNested()
-    @Type(() => ObservationDto)
-    observation: ObservationDto;
+  @ApiProperty()
+  @ValidateNested()
+  @Type(() => LearningAssessmentDto)
+  learningAssessment: LearningAssessmentDto;
 
-    @ApiProperty()
-    @ValidateNested()
-    @Type(() => LearningAssessmentDto)
-    learningAssessment: LearningAssessmentDto;
-   
-    @ApiProperty()
-    @IsString()
-    @IsOptional()
-    therapistInCharge: string;
+  @ApiProperty()
+  @IsString()
+  @IsOptional()
+  therapistInCharge: string;
 
-    @ApiProperty()
-    @IsString()
-    @IsOptional()
-    informationOfficer: string;
+  @ApiProperty()
+  @IsString()
+  @IsOptional()
+  informationOfficer: string;
 
-    @ApiProperty()
-    @IsString()
-    @IsOptional()
-    choiseInformationOfficer: string;
+  @ApiProperty()
+  @IsString()
+  @IsOptional()
+  choiseInformationOfficer: string;
 }
--- src/features/psychopedagogical-anamnesis/types/psychopedagogicalAnamnesis.ts
+++ src/features/psychopedagogical-anamnesis/types/psychopedagogicalAnamnesis.ts
@@ -1,94 +1,89 @@
-export interface IPsychopedagogicalAnamnesis{
+export interface IPsychopedagogicalAnamnesis {
+  patient: string; // ObjectId as string
+  dateOfApplication: string;
+  dateBirth: string;
+  age: number;
+  teacherName: string;
+  supportProfessional: string;
+  schoolName: string;
+  schoolPeriod: string;
+  schoolYear: string;
 
-    patient: string; // ObjectId as string
-    dateOfApplication: string;
-    dateBirth: string;
-    age: number;
-    teacherName: string;
-    supportProfessional: string;
-    schoolName: string;
-    schoolPeriod: string;
-    schoolYear: string;
+  familyLearningHistory: {
+    fatherName: string;
+    ageFather: number;
+    schoolingFather: string;
+    difficultyFather: string;
+    motherName: string;
+    ageMother: number;
+    schoolingMother: string;
+    difficultyMother: string;
+    nameBrotherOne: string;
+    ageBrotherOne: number;
+    schoolingBrotherOne: string;
+    difficultyBrotherOne: string;
+    nameBrotherTwo: string;
+    ageBrotherTwo: number;
+    schoolingBrotherTwo: string;
+    difficultyBrotherTwo: string;
+    nameBrotherThree: string;
+    ageBrotherThree: number;
+    schoolingBrotherThree: string;
+    difficultyBrotherThree: string;
+  };
 
+  observation: {
+    bedtime: string;
+    wakeUpTime: string;
+    sportsActivitiesHeLikesTheMost: string;
+    externalServiceActivities: string;
+    doYouUseAdaptedActivitiesAtSchool: string;
+    whatActivitiesAreAdapted: string;
+    doYouHaveDifficultyInSocialInteraction: string;
+    whatDifficultyInSocialInteraction: string;
+    resistanceToRoutineChange: string;
+    whatRoutineChange: string;
+    eyeContact: string;
+    whatEyeContact: string;
+    interestInLearning: string;
+    whatInterestInLearning: string;
+    hyperactivity: string;
+    whatHyperactivity: string;
+    inattention: string;
+    whatInattention: string;
+    areYouLiterate: string;
+    whatAreYouLiterate: string;
+    changeInLanguageAndCommunication: string;
+    whatLanguageAndCommunication: string;
+    inappropriateReactionToFrustration: string;
+    whatReactionToFrustration: string;
+    crisisTypeCrisisTrigger: string;
+    whatTypeCrisisTrigger: string;
+    stereotypy: string;
+    whatStereotypy: string;
+    medication: string;
+    whatMedication: string;
+    allergy: string;
+    whatAllergy: string;
+    Diagnosis: string;
+    schoolFamilyComplaint: string;
+    outher: string;
+  };
 
-    familyLearningHistory:{
-        fatherName: string;
-        ageFather: number;
-        schoolingFather: string;
-        difficultyFather: string;
-        motherName: string;
-        ageMother: number;
-        schoolingMother: string;
-        difficultyMother: string;
-        nameBrotherOne: string;
-        ageBrotherOne: number;
-        schoolingBrotherOne: string;
-        difficultyBrotherOne: string;
-        nameBrotherTwo: string;
-        ageBrotherTwo: number;
-        schoolingBrotherTwo: string;
-        difficultyBrotherTwo: string;
-        nameBrotherThree: string;
-        ageBrotherThree: number;
-        schoolingBrotherThree: string;
-        difficultyBrotherThree: string;
-    }
+  learningAssessment: {
+    readingLevel: string;
+    textProduction: string;
+    preSyllabicWritingLevel: string;
+    doYouRecognizeTheColors: string;
+    difficultyRecognizeTheColors: string;
+    doYouHaveDifficultyTracingTheNumerals: string;
+    difficultyRecognizingNumbers: string;
+    doYouHaveAChangeWhenCarryingOutOperations: string;
+    difficultyCarryingOutOperations: string;
+    doesItSolveProblemSituations: string;
+  };
 
-    observation:{
-        bedtime: number;
-        wakeUpTime: number;
-        sportsActivitiesHeLikesTheMost: string;
-        externalServiceActivities: string;
-        doYouUseAdaptedActivitiesAtSchool: boolean;
-        whatActivitiesAreAdapted: string;
-        doYouHaveDifficultyInSocialInteraction: boolean;
-        whatDifficultyInSocialInteraction: boolean;
-        resistanceToRoutineChange: boolean;
-        whatRoutineChange: string;
-        eyeContact: boolean;
-        whatEyeContact: string;
-        interestInLearning: boolean;
-        whatInterestInLearning: string;
-        hyperactivity: boolean;
-        whatHyperactivity: string;
-        inattention: boolean;
-        whatInattention: string;
-        areYouLiterate: boolean;
-        whatAreYouLiterate: string;
-        changeInLanguageAndCommunication: boolean;
-        whatLanguageAndCommunication: string;
-        inappropriateReactionToFrustration: boolean;
-        whatReactionToFrustration: string;
-        crisisTypeCrisisTrigger: boolean;
-        whatTypeCrisisTrigger: string;
-        stereotypy: boolean;
-        whatStereotypy: string;
-        medication: boolean;
-        whatMedication: string;
-        allergy: boolean;
-        whatAllergy: string;
-        Diagnosis: string;
-        schoolFamilyComplaint: string;
-        outher: string;
-    }
-
-
-
-    learningAssessment:{
-        readingLevel: string;
-        textProduction: string;
-        preSyllabicWritingLevel: string;
-        doYouRecognizeTheColors: boolean;                                                                        
-        difficultyRecognizeTheColors: boolean;
-        doYouHaveDifficultyTracingTheNumerals: boolean;
-        difficultyRecognizingNumbers: boolean;
-        doYouHaveAChangeWhenCarryingOutOperations: string;
-        difficultyCarryingOutOperations: boolean;
-        doesItSolveProblemSituations: boolean;
-    }
-
-       
-    therapistInCharge: string; 
-    informationOfficer: string;
-    choiseInformationOfficer: string;
+  therapistInCharge: string;
+  informationOfficer: string;
+  choiseInformationOfficer: string;
 }
