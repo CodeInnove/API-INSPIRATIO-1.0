345bcb29c26308b9008f4be45f3dde74e4be5900
Juan Guillherme <guilhermejuan729@gmail.com>
2025-01-21 16:47:54 -0300

    excluindo arquivo desnecessario

--- src/features/income-assessment/dto/updateIncomeAssessment.dto.ts
+++
@@ -1,4 +0,0 @@
-import { PartialType } from '@nestjs/swagger';
-import { CreateIncomeAssessmentDto } from './createIncomeAssessment.dto';
-
-export class UpdateIncomeAssessmentDto extends PartialType(CreateIncomeAssessmentDto) {}
--- src/features/income-assessment/incomeAssessment.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-import { IncomeAssessmentService } from './incomeAssessment.service';
-import { IncomeAssessmentController } from './incomeAssessment.controller';
-
-@Module({
-  controllers: [IncomeAssessmentController],
-  providers: [IncomeAssessmentService],
-})
-export class IncomeAssessmentModule {}
