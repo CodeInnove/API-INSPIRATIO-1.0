1afbedeff085fc4550badd91b7510df849e7094c
LucasVannucchi <lucasvannucchi16@gmail.com>
2025-01-21 23:23:06 -0300

    arrumando o back-end OTA

--- src/features/occupational-therapy-anamnesis/dto/createOccupationalTherapyAnamnesis.dto.ts
+++ src/features/occupational-therapy-anamnesis/dto/createOccupationalTherapyAnamnesis.dto.ts
@@ -1,187 +1,172 @@
-import { ApiProperty } from "@nestjs/swagger";
+import { ApiProperty } from '@nestjs/swagger';
 
 export class CreateOccupationalTherapyAnamnesisDto {
-    @ApiProperty()
-    dateOfApplication: Date;
+  @ApiProperty()
+  dateOfApplication: Date;
 
-    @ApiProperty()
-    doctor: string;
+  @ApiProperty()
+  doctor: string;
 
-    @ApiProperty()
-    patient: string;
+  @ApiProperty()
+  patient: string;
 
-    @ApiProperty()
-    birthDate: Date;
+  @ApiProperty()
+  birthDate: Date;
 
-    @ApiProperty()
-    gender: string
+  @ApiProperty()
+  gender: string;
 
-    @ApiProperty()
-    age: number;
+  @ApiProperty()
+  age: number;
 
-    @ApiProperty()
-    diagnoses: string;
+  @ApiProperty()
+  diagnoses: string;
 
-    @ApiProperty()
-    fatherName: string;
+  @ApiProperty()
+  fatherName: string;
 
-    @ApiProperty()
-    motherName: string;
+  @ApiProperty()
+  motherName: string;
 
-    @ApiProperty()
-    contactPhone: string;
+  @ApiProperty()
+  contactPhone: string;
 
-    @ApiProperty()
-    address: string;
+  @ApiProperty()
+  address: string;
 
-    @ApiProperty()
-    consultationReason: string;
-    @ApiProperty()
-    mainConcernOfResponsible: string;
+  @ApiProperty()
+  consultationReason: string;
+  @ApiProperty()
+  mainConcernOfResponsible: string;
 
+  @ApiProperty()
+  pregnancy: string;
+  @ApiProperty()
+  complicationsDuringPregnancy: string;
+  @ApiProperty()
+  birth: string;
+  @ApiProperty()
+  prematurity: string;
+  @ApiProperty()
+  gestationalWeeks: number;
+  @ApiProperty()
+  apgarAtBirth: number;
+  @ApiProperty()
+  neonatalHospitalization: string;
+  @ApiProperty()
+  hospitalizationTime: string;
+  @ApiProperty()
+  pastDiseasesOrHospitalizations: string;
 
+  @ApiProperty()
+  headControlMonths: number;
+  @ApiProperty()
+  sittingWithoutSupportMonths: number;
+  @ApiProperty()
+  crawlingMonths: number;
+  @ApiProperty()
+  walkingMonths: number;
+  @ApiProperty()
+  firstWordsMonths: number;
+  @ApiProperty()
+  sphincterControlYears: number;
+  @ApiProperty()
+  regressionOrLossOfSkills: string;
 
-    @ApiProperty()
-    pregnancy: string;
-    @ApiProperty()
-    complicationsDuringPregnancy: string;
-    @ApiProperty()
-    birth: string;
-    @ApiProperty()
-    prematurity: string;
-    @ApiProperty()
-    gestationalWeeks: number;
-    @ApiProperty()
-    apgarAtBirth: number;
-    @ApiProperty()
-    neonatalHospitalization: string;
-    @ApiProperty()
-    hospitalizationTime: string;
-    @ApiProperty()
-    pastDiseasesOrHospitalizations: string;
+  @ApiProperty()
+  feeding: string;
+  @ApiProperty()
+  feedingHelpDetails: string;
+  @ApiProperty()
+  aversionToCertainFoods: string;
+  @ApiProperty()
+  aversionFoodDetails: string;
+  @ApiProperty()
+  acceptsNewFoodsEasily: string;
+  @ApiProperty()
+  needsSpecificStrategiesForFeeding: string;
+  @ApiProperty()
+  feedingStrategies: string;
+  @ApiProperty()
+  prefersSpecificFoodConsistency: string;
+  @ApiProperty()
+  personalHygiene: string;
+  @ApiProperty()
+  hygieneDetails: string;
+  @ApiProperty()
+  bathing: string;
+  @ApiProperty()
+  bathSpecificDetails: string;
+  @ApiProperty()
+  dressing: string;
+  @ApiProperty()
+  dressingDifficultyDetails: string;
+  @ApiProperty()
+  sphincterControl: string;
+  @ApiProperty()
+  specificSituationsForDiaper: string;
+  @ApiProperty()
+  inToiletTraining: string;
+  @ApiProperty()
+  toiletTrainingDuration: string;
+  @ApiProperty()
+  strategiesUsedByCaregiversOrSchool: string;
 
+  @ApiProperty()
+  signsOfReadiness: string;
+  @ApiProperty()
+  specificDifficultiesInToiletTraining: string;
 
+  @ApiProperty()
+  participatesInSchoolOrHomeTasks: string;
+  @ApiProperty()
+  taskDetails: string;
 
-    @ApiProperty()
-    headControlMonths: number;
-    @ApiProperty()
-    sittingWithoutSupportMonths: number;
-    @ApiProperty()
-    crawlingMonths: number;
-    @ApiProperty()
-    walkingMonths: number;
-    @ApiProperty()
-    firstWordsMonths: number;
-    @ApiProperty()
-    sphincterControlYears: number;
-    @ApiProperty()
-    regressionOrLossOfSkills: string;
+  @ApiProperty()
+  enrolledInSchool: string;
+  @ApiProperty()
+  enrolledInSchoolDetails: string;
+  @ApiProperty()
+  educationType: string;
+  @ApiProperty()
+  difficultiesReportedByTeachers: string;
 
+  @ApiProperty()
+  participatesInGroupRecreationalActivities: string;
+  @ApiProperty()
+  interactionWithOtherChildrenAndAdults: string;
 
+  @ApiProperty()
+  sensorySensitivities: string;
+  @ApiProperty()
+  taskDetailsSensitivities: string;
+  @ApiProperty()
+  repetitiveOrStereotypedBehaviors: string;
+  @ApiProperty()
+  repetitiveBehaviorsDescription: string;
+  @ApiProperty()
+  responsesToRoutineChanges: string;
+  @ApiProperty()
+  behaviorCrises: string;
+  @ApiProperty()
+  crisesDetails: string;
 
-    @ApiProperty()
-    feeding: string;
-    @ApiProperty()
-    feedingHelpDetails: string;
-    @ApiProperty()
-    aversionToCertainFoods: string;
-    @ApiProperty()
-    aversionFoodDetails: string;
-    @ApiProperty()
-    acceptsNewFoodsEasily: string;
-    @ApiProperty()
-    needsSpecificStrategiesForFeeding: string;
-    @ApiProperty()
-    feedingStrategies: string;
-    @ApiProperty()
-    prefersSpecificFoodConsistency: string;
-    @ApiProperty()
-    personalHygiene: string;
-    @ApiProperty()
-    hygieneDetails: string;
-    @ApiProperty()
-    bathing: string;
-    @ApiProperty()
-    bathSpecificDetails: string;
-    @ApiProperty()
-    dressing: string;
-    @ApiProperty()
-    dressingDifficultyDetails: string;
-    @ApiProperty()
-    sphincterControl: string;
-    @ApiProperty()
-    specificSituationsForDiaper: string;
-    @ApiProperty()
-    inToiletTraining: string;
-    @ApiProperty()
-    toiletTrainingDuration: string;
-    @ApiProperty()
-    strategiesUsedByCaregiversOrSchool: string;
-    
+  @ApiProperty()
+  prefersStructuredOrFreePlay: string;
+  @ApiProperty()
+  favoriteToysOrActivities: string;
+  @ApiProperty()
+  difficultyStartingOrMaintainingPlay: string;
 
+  @ApiProperty()
+  previousTherapies: string;
+  @ApiProperty()
+  previousTherapiesDetails: string;
+  @ApiProperty()
+  familyOrCaregiversInTherapies: string;
 
-
-    @ApiProperty()
-    signsOfReadiness: string;
-    @ApiProperty()
-    specificDifficultiesInToiletTraining: string;
-
-
-
-    @ApiProperty()
-    participatesInSchoolOrHomeTasks:string;
-    @ApiProperty()
-    taskDetails: string;
-
-
-
-    @ApiProperty()
-    enrolledInSchool: string;
-    @ApiProperty()
-    educationType: string;
-
-
-    @ApiProperty()
-    participatesInGroupRecreationalActivities: string;
-    @ApiProperty()
-    interactionWithOtherChildrenAndAdults: string;
-
-
-
-    @ApiProperty()
-    sensorySensitivities: string;
-    @ApiProperty()
-	taskDetailsSensitivities: string;
-    @ApiProperty()
-    repetitiveOrStereotypedBehaviors: string;
-    @ApiProperty()
-    repetitiveBehaviorsDescription: string;
-    @ApiProperty()
-    responsesToRoutineChanges: string;
-    @ApiProperty()
-    behaviorCrises: string;
-    @ApiProperty()
-    crisesDetails: string;
-
-
-    @ApiProperty()
-    prefersStructuredOrFreePlay:  string;
-    @ApiProperty()
-    favoriteToysOrActivities: string;
-    @ApiProperty()
-    difficultyStartingOrMaintainingPlay: string;
-
-
-
-
-    @ApiProperty()
-    previousTherapies: string;
-    @ApiProperty()
-    familyOrCaregiversInTherapies: string;
-
-
-    @ApiProperty()
-    essentialObservationsForTreatmentPlanning: string;
-    @ApiProperty()
-    responsibleObservations: string;
+  @ApiProperty()
+  essentialObservationsForTreatmentPlanning: string;
+  @ApiProperty()
+  responsibleObservations: string;
 }
--- src/features/occupational-therapy-anamnesis/types/occupationalTherapyAnamnesis.ts
+++ src/features/occupational-therapy-anamnesis/types/occupationalTherapyAnamnesis.ts
@@ -1,96 +1,122 @@
-
-
 export interface IGeneralObservations {
-    consultationReason: string;
-    mainConcernOfResponsible: string;
+  consultationReason: string;
+  mainConcernOfResponsible: string;
 }
 
 export interface IClinicalHistory {
-    pregnancy: 'Planejada' | 'Não planejada';
-    complicationsDuringPregnancy: string;
-    birth: 'Normal' | 'Cesárea';
-    prematurity: 'Sim' | 'Não';
-    gestationalWeeks: number;
-    apgarAtBirth: number;
-    neonatalHospitalization: 'Sim' | 'Não';
-    hospitalizationTime: string;
-    pastDiseasesOrHospitalizations: string;
+  pregnancy: 'Planejada' | 'Não planejada';
+  complicationsDuringPregnancy: string;
+  birth: 'Normal' | 'Cesárea';
+  prematurity: 'Sim' | 'Não';
+  gestationalWeeks: number;
+  apgarAtBirth: number;
+  neonatalHospitalization: 'Sim' | 'Não';
+  hospitalizationTime: string;
+  pastDiseasesOrHospitalizations: string;
 }
 
 export interface INeuropsychomotorDevelopment {
-    headControlMonths: number;
-    sittingWithoutSupportMonths: number;
-    crawlingMonths: number;
-    walkingMonths: number;
-    firstWordsMonths: number;
-    sphincterControlYears: number;
-    regressionOrLossOfSkills: string;
+  headControlMonths: number;
+  sittingWithoutSupportMonths: number;
+  crawlingMonths: number;
+  walkingMonths: number;
+  firstWordsMonths: number;
+  sphincterControlYears: number;
+  regressionOrLossOfSkills: string;
 }
 
 export interface IActivitiesOfDailyLiving {
-    feeding: 'Independente' | 'Necessita de auxílio' | 'Alimentação por sonda';
-    feedingHelpDetails: string;
-    aversionToCertainFoods: 'Sim' | 'Não';
-    aversionFoodDetails: string;
-    acceptsNewFoodsEasily: 'Sim' | 'Não';
-    needsSpecificStrategiesForFeeding: 'Sim' | 'Não';
-    feedingStrategies: string;
-    prefersSpecificFoodConsistency: string;
-    personalHygiene: 'Independente' | 'Com auxílio parcial' | 'Necessita de supervisão total';
-    hygieneDetails: string;
-    bathing: 'Independente' | 'Auxílio para partes específicas (especificar)' | 'Necessita de supervisão completa';
-    bathSpecificDetails: string;
-    dressing: 'Completa independência' | 'Dificuldade com botões, zíperes, etc' | 'Auxílio completo';
-    dressingDifficultyDetails: string;
-    sphincterControl: 'Total' | 'Parcial (usa fralda em determinadas situações)' | 'Necessita de fralda o tempo todo';
-    specificSituationsForDiaper: string;
-    inToiletTraining: 'Sim' | 'Não';
-    toiletTrainingDuration: string;
-    strategiesUsedByCaregiversOrSchool: string;
+  feeding: 'Independente' | 'Necessita de auxílio' | 'Alimentação por sonda';
+  feedingHelpDetails: string;
+  aversionToCertainFoods: 'Sim' | 'Não';
+  aversionFoodDetails: string;
+  acceptsNewFoodsEasily: 'Sim' | 'Não';
+  needsSpecificStrategiesForFeeding: 'Sim' | 'Não';
+  feedingStrategies: string;
+  prefersSpecificFoodConsistency: string;
+  personalHygiene:
+    | 'Independente'
+    | 'Com auxílio parcial'
+    | 'Necessita de supervisão total';
+  hygieneDetails: string;
+  bathing:
+    | 'Independente'
+    | 'Auxílio para partes específicas (especificar)'
+    | 'Necessita de supervisão completa';
+  bathSpecificDetails: string;
+  dressing:
+    | 'Completa independência'
+    | 'Dificuldade com botões, zíperes, etc'
+    | 'Auxílio completo';
+  dressingDifficultyDetails: string;
+  sphincterControl:
+    | 'Total'
+    | 'Parcial (usa fralda em determinadas situações)'
+    | 'Necessita de fralda o tempo todo';
+  specificSituationsForDiaper: string;
+  inToiletTraining: 'Sim' | 'Não';
+  toiletTrainingDuration: string;
+  strategiesUsedByCaregiversOrSchool: string;
 }
 
 export interface ISignsOfReadiness {
-    signsOfReadiness: 'Percepção de xixi/cocô' | 'Desconforto com a fralda' | 'Ser capaz de apontar o que deseja' | 'Ficar seco por 2h ou mais' | 'Seguir comandos' | 'Ficar sentado de 3 a 5 minutos';
-    specificDifficultiesInToiletTraining: string;
+  signsOfReadiness:
+    | 'Percepção de xixi/cocô'
+    | 'Desconforto com a fralda'
+    | 'Ser capaz de apontar o que deseja'
+    | 'Ficar seco por 2h ou mais'
+    | 'Seguir comandos'
+    | 'Ficar sentado de 3 a 5 minutos';
+  specificDifficultiesInToiletTraining: string;
 }
 
 export interface IInstrumentalActivitiesOfDailyLiving {
-    participatesInSchoolOrHomeTasks: 'Sim' | 'Não';
-    taskDetails: string;
+  participatesInSchoolOrHomeTasks: 'Sim' | 'Não';
+  taskDetails: string;
 }
 
 export interface ISchoolAndSocialRoutine {
-    enrolledInSchool: 'Sim' | 'Não';
-    educationType: 'Regular' | 'Regular com apoio' | 'Especial';
+  enrolledInSchool: 'Sim' | 'Não';
+  enrolledInSchoolDetails: string;
+  educationType: 'Regular' | 'Regular com apoio' | 'Especial';
+  difficultiesReportedByTeachers: string;
 }
 
 export interface ITeacherReportedDifficulties {
-    participatesInGroupRecreationalActivities: 'Sim' | 'Não';
-    interactionWithOtherChildrenAndAdults: 'Busca interação' | 'Evita interação' | 'Prefere brincar sozinho';
+  participatesInGroupRecreationalActivities: 'Sim' | 'Não';
+  interactionWithOtherChildrenAndAdults:
+    | 'Busca interação'
+    | 'Evita interação'
+    | 'Prefere brincar sozinho';
 }
 
 export interface ISensoryAndBehavioralAspects {
-    sensorySensitivities: 'Tátil' | 'Auditiva' | 'Visual' | 'Olfativa' | 'Outras';
-	taskDetailsSensitivities: string;
-    repetitiveOrStereotypedBehaviors: 'Sim' | 'Não';
-    repetitiveBehaviorsDescription: string;
-    responsesToRoutineChanges: 'Boa adaptação' | 'Dificuldade significativa';
-    behaviorCrises: 'Sim' | 'Não';
-    crisesDetails: string;
+  sensorySensitivities: 'Tátil' | 'Auditiva' | 'Visual' | 'Olfativa' | 'Outras';
+  taskDetailsSensitivities: string;
+  repetitiveOrStereotypedBehaviors: 'Sim' | 'Não';
+  repetitiveBehaviorsDescription: string;
+  responsesToRoutineChanges: 'Boa adaptação' | 'Dificuldade significativa';
+  behaviorCrises: 'Sim' | 'Não';
+  crisesDetails: string;
 }
 
 export interface IPlayAndLeisureAspects {
-    prefersStructuredOrFreePlay:  'Estruturadas' | 'Livres';
-    favoriteToysOrActivities: string;
-    difficultyStartingOrMaintainingPlay: 'Sim' | 'Não';
+  prefersStructuredOrFreePlay: 'Estruturadas' | 'Livres';
+  favoriteToysOrActivities: string;
+  difficultyStartingOrMaintainingPlay: 'Sim' | 'Não';
 }
 
 export interface ISupportNetworkAndPreviousTreatments {
-    previousTherapies: 'Fonoaudiologia' | 'Fisioterapia' | 'Psicologia' | 'Outros';
-    familyOrCaregiversInTherapies: string;
+  previousTherapies:
+    | 'Fonoaudiologia'
+    | 'Fisioterapia'
+    | 'Psicologia'
+    | 'Outros';
+  previousTherapiesDetails: string;
+  familyOrCaregiversInTherapies: string;
 }
 
 export interface IResponsibleGeneralObservations {
-    essentialObservationsForTreatmentPlanning: 'Sim' | 'Não';
-    responsibleObservations: string;
+  essentialObservationsForTreatmentPlanning: 'Sim' | 'Não';
+  responsibleObservations: string;
 }
