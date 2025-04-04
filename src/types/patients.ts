4ad8c6202cead0b6bc02747652574b980a1d6b69
Juan Moraes <130096510+DevMoraesss@users.noreply.github.com>
2025-04-04 10:46:31 -0300

    Update patients.ts

--- src/types/patients.ts
+++ src/types/patients.ts
@@ -2,10 +2,10 @@ import { Doctor } from "./doctor";
 import { Responsible } from "./responsible";
 
 export interface Patients{
-  _id: string; // Identificador do paciente
-  name: string; // Nome do paciente
-  age: number; // Idade do paciente
-  doctor: Doctor; // Referência ao doutor (psicólogo/médico responsável)
-  responsible: Responsible; // Referência ao responsável (obrigatório se o paciente for menor)
-  isSelfResponsible: boolean; // Indica se o paciente é responsável por si mesmo
+  _id: string; 
+  name: string; 
+  age: number; 
+  doctor: Doctor; 
+  responsible: Responsible; 
+  isSelfResponsible: boolean; 
 }
